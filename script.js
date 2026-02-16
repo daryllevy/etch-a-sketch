4ab1277ddff40e4ae71ab13b5cd5e9ba02a7f3ff
Levy Daryl <222512535+daryllevy@users.noreply.github.com>
2026-02-16 10:53:26 +0100

    ajout des divs et d'un petit style

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    let div = document.createElement("div");
+    div.setAttribute(
+      "style",
+      "height: 16px; width: 16px; background-color: green; margin: 10px 8px;",
+    );
+
+    container.appendChild(div);
+  }
+}
