32e5a88d6fa85248219d57c15ce7c6b1273a6b29
Levy Daryl <222512535+daryllevy@users.noreply.github.com>
2026-02-17 12:34:27 +0100

    ajout de la logique pour une grille dynamique

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    let div = document.createElement("div");
-    div.setAttribute(
-      "style",
-      "width: calc(100% / 16); height: calc(100% / 16); border: 1px solid green; ",
-    );
+///Fonction pour créer la grille
+function createGrid(number) {
+  for (let i = 0; i < number; i++) {
+    for (let j = 0; j < number; j++) {
+      let div = document.createElement("div");
+      div.setAttribute(
+        "style",
+        `width: calc(100% / ${number}); height: calc(100% / ${number}); border: 1px solid green; `,
+      );
 
-    container.appendChild(div);
+      div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "#ccc";
+      });
+      container.appendChild(div);
+    }
   }
 }
+
+button.addEventListener("click", () => {
+  const numberGrid = parseInt(
+    prompt("How much square per side do you want ? (between 0 and 100) : "),
+  );
+
+  console.log(numberGrid);
+  container.textContent = "";
+  createGrid(numberGrid);
+});
